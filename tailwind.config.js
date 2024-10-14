12a8201d32d378f3b4284e346fe5c0cad9bb78a1
unknown <tegar.narendra@gmail.com>
2024-10-14 15:02:38 +0700

    warna navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./page/**/*.{html,js}'],
+  content: ["./page/**/*.{html,js}"],
   theme: {
     extend: {
-      colors:{
-        bluey:"#64CCC5",
-        lightbluey:"#DAFFFB"
+      colors: {
+        bluey: "#64CCC5",
+        lightbluey: "#DAFFFB",
+        ijotaik: "#04364A",
+        ijoagakmuda: "#38A3A5",
+        greenwhitey: "#CDE0C9",
       },
       fontFamily: {
-        inter : ["Inter", "sans-serif"],
-        poppins : ["poppins","sans-serif"],
+        inter: ["Inter", "sans-serif"],
+        poppins: ["poppins", "sans-serif"],
+        archivo: ["Archivo Black", "sans-serif"],
+      },
     },
   },
-},
   plugins: [],
-}
-
+};
